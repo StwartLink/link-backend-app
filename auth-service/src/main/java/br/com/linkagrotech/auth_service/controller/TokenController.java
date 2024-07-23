b45496d91fe68cc254e6d39defd055631f062793
Stwart Massmann <stwart.martins@linkgestao.com.br>
2024-07-23 08:53:00 -0300

    Ajustado keycloak

--- auth-service/src/main/java/br/com/linkagrotech/auth_service/controller/TokenController.java
+++ auth-service/src/main/java/br/com/linkagrotech/auth_service/controller/TokenController.java
@@ -10,39 +10,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.extern.slf4j.Slf4j;
 
-@RequestMapping("/token")
+@RequestMapping("/")
 @RestController
+@Slf4j
 public class TokenController {
 
     @Operation(summary = "Request Token", description = "Login ", security = @SecurityRequirement(name = "security_auth"))
-    @PostMapping
+    @PostMapping("/token")
     public ResponseEntity<String> token(@RequestBody User user) {
-
         HttpHeaders headers = new HttpHeaders();
         RestTemplate rt = new RestTemplate();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("client_id", "microservices");
-        formData.add("username", user.username);
-        formData.add("password", user.password);
+        formData.add("username", user.username());
+        formData.add("password", user.password());
         formData.add("grant_type", "password");
 
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(formData,
-                headers);
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(formData, headers);
 
-        var result = rt.postForEntity("http://localhost:8081/realms/agrobrasil/protocol/openid-connect/token", entity,
-                String.class);
-
-        return result;
+        try {
+            var result = rt.postForEntity("http://localhost:8081/realms/agrobrasil/protocol/openid-connect/token",
+                    entity,
+                    String.class);
+            return result;
+        } catch (HttpClientErrorException e) {
+            log.error("HttpClientErrorException: {} - {}", e.getStatusCode(), e.getResponseBodyAsString());
+            return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
+        } catch (Exception e) {
+            log.error("Exception: ", e);
+            return ResponseEntity.status(500).body("Internal Server Error");
+        }
     }
 
-    public record User(String password, String username) {
+    public record User(String username, String password) {
     }
-
 }
